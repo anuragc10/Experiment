d8e98e803f77c7ca81b6d5eaabb01e34e092204f
Anurag Singh <63997049+anuragc10@users.noreply.github.com>
2022-10-02 01:03:39 +0530

    Create Next Permutation.cpp

---
+++ Next Permutation.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int k;
+        for(k=n-2;k>=0;k--)
+        {
+            if(nums[k]<nums[k+1])
+            {
+                break;
+            }
+        }
+        if(k<0)
+        {
+            reverse(nums.begin(),nums.end());
+        }
+        else
+        {
+            for(int i=n-1;i>=0;i--)
+            {
+                if(nums[i]>nums[k])
+                {
+                    swap(nums[i],nums[k]);
+                    break;
+                }
+            }
+            reverse(nums.begin()+k+1,nums.end());
+        }
+    }
+};
